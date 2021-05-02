b13c729f90d26247baca5e9c62fa230c73419b0f
Vince Lu <vincentlu@hotmail.ca>
2021-05-02 19:28:57 -0400

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCo9r0mIeks-21s1qHL3rneDYP5idZ8YZc",
+    authDomain: "expo--login-651d4.firebaseapp.com",
+	databaseURL: "https://expo--login-651d4-default-rtdb.firebaseio.com/",
+    projectId: "expo--login-651d4",
+    storageBucket: "expo--login-651d4.appspot.com",
+    messagingSenderId: "644671780764",
+    appId: "1:644671780764:web:7877fe8b94bcfb16277a59"
+};
---
+++ screens/LoadingScreen.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import firebase from 'firebase';
+
+class LoadingScreen extends Component {
+	
+	componentDidMount(){
+		this.checkIfLoggedIn();
+	}
+	
+	checkIfLoggedIn = () => {
+		firebase.auth().onAuthStateChanged(function(user){
+			if(user) {
+				this.props.navigation.navigate('DashboardScreen');
+			} else {
+				this.props.navigation.navigate('LoginScreen');
+			}
+		}.bind(this));
+	};
+	
+	render() {
+		return (
+			<View style={styles.container}>
+				<ActivityIndicator size="large" color="#0000ff" />
+			</View>
+		);
+	}
+}
+export default LoadingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
